41ef730ff949dbc43ec0ba3acc5749595b52b700
Ryosuke Nakashiro <ryo@zukan.com>
2017-06-14 18:22:31 +0900

    fix: fix optional description.

--- swagger_himotoki/himotoki_decodable.swift
+++ swagger_himotoki/himotoki_decodable.swift
@@ -15,6 +15,7 @@ struct {{ definition.class_name }}: Decodable {
     let {{ property.key_name | camelize }}: {% if property.is_array -%} [ {%- endif -%}
         {{  property.property_type | swift_type(property.class_name_ref) }}
         {%- if property.is_array -%} ] {%- endif %}
+        {%- if not property.is_required -%} ? {%- endif %}
     {%- endfor %}
 
     static func decode(_ ext: Extractor) throws -> {{ definition.class_name }} {
